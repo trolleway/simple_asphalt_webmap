edf472a16d9eaab70131e76546f55cfb01b2e885
Artem Svetlov <trolleway@yandex.ru>
2019-06-07 18:53:09 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+
+
+filename=central-fed-district-latest
+wget -t http://download.geofabrik.de/russia/$filename.osm.pbf
+
+osmconvert $filename.osm.pbf -o=$filename.o5m
+osmfilter $filename.o5m --drop-author --keep="all highway="   --out-o5m >$filename-filtered.o5m
+osmconvert $filename-filtered.o5m -o=$filename-filtered.pbf
